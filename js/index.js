4c3bc62093bcd1a051f0505d238e3bde095248cf
wilber <wilber_nilber@hotmail.com>
2020-07-29 19:06:40 -0300

    Incluindo arquivo js

---
+++ js/index.js
@@ -0,0 +1,32 @@
+console.log('Olá Mundo!');
+
+let var1 = 1;
+let var2 = -1;
+let var3 = 1.1;
+let var4 = 'var4';
+let var5 = true;
+
+console.log(`var1: ${var1}`);
+console.log(`var2: ${var2}`);
+console.log(`var3: ${var3}`);
+console.log(`var4: ${var4}`);
+console.log(`var5: ${var5}`);
+
+const nome = 'Wilber';
+const sobrenome = 'Silva';
+
+console.log(`${nome} ${sobrenome}`);
+
+console.log('Texto1\nTexto2');
+
+var valorNulo = null;
+var naodefinido;
+
+console.log(valorNulo);
+console.log(naodefinido);
+
+var arrayFrutas = ['Maça', 'Banana', 'Uva', 'Goiaba', 'Tomate'];
+console.log(arrayFrutas);
+
+const textoDH =
+  '“Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonharam.”';
